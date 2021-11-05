8b267e81c3eacd2ce9934b1e922f11be1f7634f8
petter <lightmango00@gmail.com>
2021-11-05 19:56:07 +0100

    back to old code

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,18 +1,11 @@
 public class LeapYear {
     public static boolean CheckLeapYear(int year) {
-        if ((year % 4 ==0) != (year % 100 == 0)) {
-            System.out.println("leap year!!: " + year);
+        if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+            System.out.println(year + " is a leap year");
+            return true;
         } else {
-            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println("leap year!!: " + year);
-                return true;
-            }  else {
-                System.out.println(year + " is not a leap year!!");
-
-            }
-
-
+            System.out.println(year +  " is not a leap year");
+            return false;
         }
-        return false;
     }
 }
